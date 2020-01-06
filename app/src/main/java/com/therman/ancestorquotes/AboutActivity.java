7780fd3838b6ce430df473d8b320e9a6d81c3aa7
tomasz-herman <tomasz.herman@protonmail.com>
2020-01-06 06:14:13 +0100

    About and readme

--- app/src/main/java/com/therman/ancestorquotes/AboutActivity.java
+++ app/src/main/java/com/therman/ancestorquotes/AboutActivity.java
@@ -10,14 +10,18 @@ import java.util.Objects;
 
 public class AboutActivity extends AppCompatActivity {
 
-    TextView tvAbout;
+    TextView tvAbout1, tvAbout2, tvAbout3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-        tvAbout = findViewById(R.id.tvAbout);
-        tvAbout.setMovementMethod(LinkMovementMethod.getInstance());
+        tvAbout1 = findViewById(R.id.tvAbout1);
+        tvAbout1.setMovementMethod(LinkMovementMethod.getInstance());
+        tvAbout2 = findViewById(R.id.tvAbout2);
+        tvAbout2.setMovementMethod(LinkMovementMethod.getInstance());
+        tvAbout3 = findViewById(R.id.tvAbout3);
+        tvAbout3.setMovementMethod(LinkMovementMethod.getInstance());
         Objects.requireNonNull(getSupportActionBar()).setTitle(getString(R.string.about));
 
     }
